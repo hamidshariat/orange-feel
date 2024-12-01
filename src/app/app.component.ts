7777d5bce9f725a63cd348de5cb6c0e0129a738c
Aria <ariaman2002@gmail.com>
2024-12-01 18:58:21 +0330

    Use mat-toolbar component in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {EditorTextComponent} from '../editor-text/editor-text.component';
+import { Component, signal } from '@angular/core';
+import { EditorTextComponent } from '../editor-text/editor-text.component';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+
+type View = 'draw' | 'editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EditorTextComponent],
+  imports: [
+    EditorTextComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuTrigger,
+    MatMenuItem,
+    MatMenu,
+  ],
   templateUrl: './app.component.html',
   standalone: true,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'orange-feel';
+
+  view = signal<View>('draw');
+
+  changeView() {
+    this.view() === 'draw' ? this.view.set('editor') : this.view.set('draw');
+  }
 }
