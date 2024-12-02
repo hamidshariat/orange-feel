b4a5a5773eeff3294d04123ac2d1f38d277f54ee
khalajabdollah <hesamkhalaj2@gmail.com>
2024-12-02 08:04:03 -0800

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EditorTextComponent} from '../editor-text/editor-text.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EditorTextComponent],
+  imports: [RouterOutlet, EditorTextComponent,MatToolbarModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'orange-feel';
-  isDarkTheme: boolean;
+  // isDarkTheme: boolean;
 
-  constructor(private themeService: ThemeService) {
-    this.isDarkTheme = this.themeService.getCurrentTheme() === 'dark-theme';
-  }
+  // constructor(private themeService: ThemeService) {
+  //   this.isDarkTheme = this.themeService.getCurrentTheme() === 'dark-theme';
+  // }
 
   // سوئیچ کردن بین تم‌ها
-  toggleTheme() {
-    if (this.isDarkTheme) {
-      this.themeService.switchTheme('light-theme');
-    } else {
-      this.themeService.switchTheme('dark-theme');
-    }
-    this.isDarkTheme = !this.isDarkTheme; // تغییر وضعیت
-  }
+  // toggleTheme() {
+  //   if (this.isDarkTheme) {
+  //     this.themeService.switchTheme('light-theme');
+  //   } else {
+  //     this.themeService.switchTheme('dark-theme');
+  //   }
+  //   this.isDarkTheme = !this.isDarkTheme; // تغییر وضعیت
+  // }
 }
